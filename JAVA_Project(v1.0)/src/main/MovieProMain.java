ea7dc82c93575b878cba9598e965f501eacc70f1
이찬해 <@chanhae1020@gmail.com>
2021-07-14 08:25:22 +0900

    과제

---
+++ JAVA_Project(v1.0)/src/main/MovieProMain.java
@@ -0,0 +1,13 @@
+package main;
+
+import frame.FrameBase;
+import frame.FrameBegin;
+
+public class MovieProMain {
+
+	public static void main(String[] args) {
+
+		FrameBase.getInstance(new FrameBegin());
+	}
+
+}
